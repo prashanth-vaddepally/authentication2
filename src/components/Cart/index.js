9dcc7e7a186796525654fd15f4bd0ae4416afe12
prashanth <prashanth.privacy@gmail.com>
2024-02-08 18:34:59 +0000

    one-case-failing

---
+++ src/components/Cart/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+      />
+      <h1>Cart</h1>
+    </div>
+  </>
+)
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+// Write your JS code here
+import Header from '../Header'
+import './index.css'
+const Products = () => (
+  <>
+    <Header />
+    <div>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+      />
+      <h1>Products</h1>
+    </div>
+  </>
+)
+export default Products
